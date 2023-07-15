2a2c526cf88c8fddf09eacaa0b1334c5802324f3
cilinyan <clyanhh@gmail.com>
2023-07-15 23:09:59 +0800

    add ablation run command

--- ablation_mask.py
+++ ablation_mask.py
@@ -9,21 +9,21 @@ python -m torch.distributed.launch --nproc_per_node=8 --master_port=59566 --use_
 """
 
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('--ngpu', default=8)
-    return parser.parse_args()
+# def get_args():
+#     parser = argparse.ArgumentParser(description='Train a detector')
+#     parser.add_argument('--ngpu', default=8)
+#     return parser.parse_args()
 
 
 def main():
-    args = get_args()
-    ngpu = args.ngpu
-    batch_size = 17 * ngpu
+    # args = get_args()
+    # ngpu = args.ngpu
+    # batch_size = 17 * ngpu
     for i in [5, 3, 1, 6, 4, 2, ]:
-        cmd = f"python -m torch.distributed.launch --nproc_per_node={ngpu} --master_port=5951{i} --use_env train.py " \
+        cmd = f"python -m torch.distributed.launch --nproc_per_node=8 --master_port=59566 --use_env train.py " \
               f"models/iter_mask/piclick_base448_cocolvis_itermask_{i}m.py " \
-              f"--batch-size={batch_size} " \
-              f"--ngpus={ngpu} " \
+              f"--batch-size=136 " \
+              f"--ngpus=8 " \
               f" | tee logs/piclick_base448_cocolvis_itermask_{i}m.log"
         os.system(cmd)
 
