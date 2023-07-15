62d8c0e58918b8064ccad71ad17f3b771aa7cdae
cilinyan <clyanhh@gmail.com>
2023-07-15 22:56:36 +0800

    add ablation run command

---
+++ ablation_mask.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+
+r"""
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=59566 --use_env train.py \
+  models/iter_mask/piclick_base448_cocolvis_itermask_5m.py \
+  --batch-size=136 \
+  --ngpus=8 
+"""
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Train a detector')
+    parser.add_argument('ngpu', default=2)
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    ngpu = args.ngpu
+    batch_size = 17 * ngpu
+    for i in [5, 3, 1, 6, 4, 2, ]:
+        cmd = f"python -m torch.distributed.launch --nproc_per_node={ngpu} --master_port=59566 --use_env train.py " \
+              f"models/iter_mask/piclick_base448_cocolvis_itermask_{i}m.py " \
+              f"--batch-size={batch_size} " \
+              f"--ngpus={ngpu} "
+        os.system(cmd)
+
+
+if __name__ == '__main__':
+    main()
